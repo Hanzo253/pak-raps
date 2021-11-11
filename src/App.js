578f04f518e2d705a404ca4c0e7bd1e5802e22a1
David Doan <42708236+Hanzo253@users.noreply.github.com>
2021-11-10 19:44:26 -0500

    created the song list (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
 import Header from "./Header";
+import SongList from "./SongList";
+import Lyrics from "./Lyrics";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <SongList />
     </div>
   );
 }
