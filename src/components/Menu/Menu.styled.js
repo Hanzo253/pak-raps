8630dd67b72ba22bbaa7eb39aecd457fbfaa2080
Hanzo253 <42708236+Hanzo253@users.noreply.github.com>
2021-11-27 03:07:02 -0500

    designed and implemented the side burger menu

---
+++ src/components/Menu/Menu.styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: rgba(55, 59, 62, 0.9);
+  //   background: ${({ theme }) => theme.primaryLight};
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+  height: 100vh;
+  text-align: left;
+  padding: 5.2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 100%;
+  }
+  a {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: ${({ theme }) => theme.primaryDark};
+    text-decoration: none;
+    transition: color 0.3s linear;
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+    &:hover {
+      color: ${({ theme }) => theme.primaryHover};
+    }
+  }
+`;
---
+++ src/theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+  primaryDark: "#fff",
+  primaryLight: "#373b3e",
+  primaryHover: "#099268",
+  mobile: "576px",
+};
