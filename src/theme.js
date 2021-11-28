922c351b7fc57b52555ae43a9900ca719bd09571
Hanzo253 <42708236+Hanzo253@users.noreply.github.com>
2021-11-28 14:49:48 -0500

    edited  some css valuues and designed a color scheme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
 export const theme = {
-  primaryDark: "#fff",
-  primaryLight: "#373b3e",
-  primaryHover: "#099268",
+  primaryDark: "#373b3e",
+  primaryLight: "#f8f9fa",
+  primaryHover: "#56b8b7",
   mobile: "576px",
 };
